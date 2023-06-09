bed78e3703eeb10d13b0ffc4d793c425576f7590
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-09 20:12:02 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+    let s1 = str.toLowerCase();
+    let s2 = s1.split('').reverse().join('');
+    if(s1===s2){
+        return true;
+    }
+    return false;
 }
 module.exports = palindrome
